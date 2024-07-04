6a3cdb78ce268a7c3064fc4a1327b25f36d0870f
AbdulMunnam07 <createrking40@gmail.com>
2024-07-04 14:27:42 +0500

    Check

---
+++ 438. Find All Anagrams in a String/find_all_anagram.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        res = []
+        if len(p) > len(s):
+            return []
+        map_s, map_p = {} , {}
+        for i in range(len(p)):
+            map_p[p[i]] = 1 + map_p.get(p[i],0)
+            map_s[s[i]] = 1 + map_s.get(s[i],0)
+        if map_p == map_s:
+            res.append(0)
+        l  = 0    # 2nd index to be return
+        for r in range(len(p) , len(s)):
+            map_s[s[r]] = 1 + map_s.get(s[r]  , 0) 
+            # addine the new ele to teh map 
+            map_s[s[l]] -=1
+            # remocing ele
+            if map_s[s[l]] == 0:
+                map_s.pop(s[l])
+            l +=1
+            if map_s == map_p:
+                res.append(l)
+        return res
+
+        '''
+        time : O(s)
+        space : O(p)
+        '''
