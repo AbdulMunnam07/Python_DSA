f488cb385e27629b7f55bf7547192333af68a64b
AbdulMunnam07 <createrking40@gmail.com>
2024-07-11 15:11:12 +0500

    Check

---
+++ 2164. Sort Even and Odd Indices Independently/Sort_even_odd_index.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortEvenOdd(self, nums: List[int]) -> List[int]:
+        odd,even = [],[]
+        for i in range(len(nums)):
+            if i % 2 ==0:
+                even.append(nums[i])
+
+            else:
+                odd.append(nums[i])
+        even.sort()
+        odd.sort(reverse=True)
+        print(odd,even)
+        res = []
+        for i in range(len(even)):
+            res.append(even[i])
+            if len(odd) > i:
+                res.append(odd[i])
+        print(res)
+        return res
