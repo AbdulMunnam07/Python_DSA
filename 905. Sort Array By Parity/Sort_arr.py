ebd413cf0e9f400895b5cc5045b3640a5a95dbd1
AbdulMunnam07 <createrking40@gmail.com>
2024-07-11 13:29:26 +0500

    Check

--- 905. Sort Array By Parity/Sort_arr.py
+++ 905. Sort Array By Parity/Sort_arr.py
@@ -1,5 +1,5 @@
 class Solution:
-    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+    def sortArrayByParity(nums):
         # arr = []
         # for i in range(len(nums)):
         #     if nums[i] % 2 == 0:
