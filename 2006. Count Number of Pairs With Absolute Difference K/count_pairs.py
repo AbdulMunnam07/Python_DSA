bf50d81447e59e2c1b4a6d59504bab0aaaeee49f
AbdulMunnam07 <createrking40@gmail.com>
2024-07-14 16:50:05 +0500

    Check

---
+++ 2006. Count Number of Pairs With Absolute Difference K/count_pairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        seen = {}
+        counter = 0
+        for num in nums:
+            if num - k in seen:
+                counter += seen[num - k]
+            if num + k in seen:
+                counter += seen[num + k]
+            if num in seen:
+                seen[num] += 1
+            else:
+                seen[num] = 1
+        return counter
+        
