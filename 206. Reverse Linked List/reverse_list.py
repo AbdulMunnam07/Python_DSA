0538f55b9d9682ac6be9e6088bd5ec5c2104a0a0
AbdulMunnam07 <createrking40@gmail.com>
2024-07-22 13:11:18 +0500

    Check

--- 206. Reverse Linked List/reverse_list.py
+++ 206. Reverse Linked List/reverse_list.py
@@ -1,5 +1,5 @@
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def reverseList(head):
         prev = None
         curr = head
         while curr:
