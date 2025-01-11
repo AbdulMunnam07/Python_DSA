e103f125520a6b02c691d01e49a69c9236357a42
Abdul Munnam <154698578+AbdulMunnam07@users.noreply.github.com>
2025-01-11 23:11:41 +0500

    Time: 51 ms (25.11%) | Memory: 18 MB (34.36%) - LeetSync

---
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if len(s) < k:
+            return False
+
+        if len(s) == k:
+            return True
+
+        freq = [0] * 26
+        odd_count = 0
+
+        for i in s:
+            freq[ord(i) - ord("a")] += 1
+
+        for count in freq:
+            if count % 2 == 1:
+                odd_count += 1
+
+        return odd_count <= k
