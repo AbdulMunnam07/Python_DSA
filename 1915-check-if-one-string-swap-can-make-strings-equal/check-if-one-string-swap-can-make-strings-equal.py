d1cc55e29abcc74f461ecffac5b72bc9044c681f
Abdul Munnam <154698578+AbdulMunnam07@users.noreply.github.com>
2025-02-05 23:46:53 +0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (47.15%) - LeetSync

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.py
@@ -0,0 +1,22 @@
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+            # If the strings are already equal, no swap is needed.
+        if s1 == s2:
+            return True
+
+        # Find the indices where s1 and s2 differ.
+        diff = []
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                diff.append(i)
+            # Early exit if more than 2 differences are found.
+            if len(diff) > 2:
+                return False
+
+        # We must have exactly 2 differences for a single swap to fix them.
+        if len(diff) != 2:
+            return False
+
+        i, j = diff
+        # Check if swapping s1[i] and s1[j] would make s1 equal to s2.
+        return s1[i] == s2[j] and s1[j] == s2[i]
