7fc02733877746d2804f353296a2962c66f8dfae
Abdul Munnam <154698578+AbdulMunnam07@users.noreply.github.com>
2025-09-15 06:44:52 +0500

    Time: 3 ms (36.59%) | Memory: 18.1 MB (16.85%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.py
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.py
@@ -1,14 +1,24 @@
 class Solution:
     def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        # Solution no. 1
+        # count = 0
+        # words = text.split(" ")
+        # for word in words:
+        #     canType = True
+        #     for ch in word:
+        #         if ch in brokenLetters:
+        #             canType = False
+        #             break
+        #     if canType:
+        #         count += 1
+        # return count
+
+        # Solution 2
+        broken = set(brokenLetters)
         count = 0
         words = text.split(" ")
         for word in words:
-            canType = True
-            for ch in word:
-                if ch in brokenLetters:
-                    canType = False
-                    break
-            if canType:
+            if all(ch not in broken for ch in word):
                 count += 1
         return count
 
