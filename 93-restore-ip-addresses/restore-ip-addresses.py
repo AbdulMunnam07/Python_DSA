05f64b9cf1702c4c9d0111d7391d355ee5b5f561
Abdul Munnam <154698578+AbdulMunnam07@users.noreply.github.com>
2025-09-16 23:44:03 +0500

    Time: 2 ms (65.37%) | Memory: 17.8 MB (48.46%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        def backtrack(start, current):
+            if len(current) == 4:
+                if start == len(s):
+                    result.append('.'.join(current))
+                return 
+            for length in range(1, 4):
+                if start + length <= len(s):
+                    segment = s[start:start + length]
+                    if self.is_valid(segment):
+                        current.append(segment)
+                        backtrack(start + length, current)
+                        current.pop()
+        result = []
+        backtrack(0, [])
+        return result
+
+    def is_valid(self, segment):
+        if len(segment) > 3 or (len(segment) > 1 and segment[0] == '0'):
+            return False
+        valid = int(segment)
+        return 0 <= valid <= 255 
